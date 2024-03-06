0ae39e78ec067cf1d1f84d2c5eca3231d18e0358
mehakkhan123 <mahekkhan284@gmail.com>
2024-03-06 16:06:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// camelCase
+var firstName = "Mehak";
+console.log(firstName);
+// snake_case
+var last_name = "Khan";
+console.log(last_name);
+// PascalCase
+var LastName = "Mehak Khan";
+console.log(LastName);
---
+++ app.ts
@@ -0,0 +1,13 @@
+// camelCase
+let firstName:string = "Mehak";
+console.log(firstName);  
+
+// snake_case
+let last_name:string = "Khan";
+console.log(last_name);
+
+// PascalCase
+let LastName:string = "Mehak Khan";
+console.log(LastName); 
+
+
